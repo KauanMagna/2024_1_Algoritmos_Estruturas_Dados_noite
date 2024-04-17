3f39586f8d50c81420b50d0a444e5ce2c9c68cec
kau <magnabosco.kka03@gmail.com>
2024-04-17 10:34:05 -0300

    mensagem aleatoria

---
+++ aula07 classes abstratass/Exercício/conta_bancaria.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Conta_bancaria(ABC):
+    def __init__(self):
+        self.nome = None 
+        self.flow = 0
+
+    @abstractmethod
+    def depositar(self, valor):
+        pass
+
+    def cadastrar(self, nome):
+        self.nome = nome
+
+        
---
+++ aula07 classes abstratass/Exercício/conta_corrente.py
@@ -0,0 +1,4 @@
+from conta_bancaria import Conta_bancaria
+
+class Conta_corrente(Conta_bancaria):
+    def __init__(self):
