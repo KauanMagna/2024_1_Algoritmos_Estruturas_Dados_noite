edff5c86ddc219f2537811bc367839a919587edf
kau <magnabosco.kka03@gmail.com>
2024-04-18 09:43:25 -0300

    atualizacao de testagem na aula

--- aula07 classes abstratass/Exercício/conta_corrente.py
+++ aula07 classes abstratass/Exercício/conta_corrente.py
@@ -1,4 +1,6 @@
 from conta_bancaria import Conta_bancaria
 
-class Conta_corrente(Conta_bancaria):
-    def __init__(self):
+#class Conta_corrente(Conta_bancaria):
+   # def __init__(self):
+
+   #testando o git
